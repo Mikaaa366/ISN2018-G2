48c3c3e78b3afec5617773a2cc9cbf1dabc12964
trututut <trururur>
2021-11-13 11:30:49 +0100

    dodano tabele klient

---
+++ UTD/database/migrations/2021_11_13_102554_create_klient_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateKlientTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('klient', function (Blueprint $table) {
+            $table->bigIncrements('id_klienta');
+            $table->string('imie', 20)->nullable(false);
+            $table->string('nazwisko', 50)->nullable(false);
+            $table->date('data_urodzenia')->nullable(false);
+            $table->integer('id_ulicy')->nullable(false);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('klient');
+    }
+}
